6841406ce08f3d94bc554e737937870aef349ad6
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-02-27 15:12:47 +0200

    Update refs.js

--- goit-js-hw-12-countries/src/js/refs.js
+++ goit-js-hw-12-countries/src/js/refs.js
@@ -1,6 +1,6 @@
 const refs = {
   searchCountry: document.querySelector('.js-search-form'),
   countryContainer: document.querySelector('.js-country'),
-  input: document.querySelector('[type="text"]'),
+  input: document.querySelector('input'),
 };
 export default refs;
